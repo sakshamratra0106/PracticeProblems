80acf435f5a150c876fe9c4ab7ba72c6937dc39e
SAKSHAM <sakshamratra.0106@gmail.com>
2022-03-11 15:53:02 +0530

    isPalindrome ?

--- AnagramPalindrome.py
+++ AnagramPalindrome.py
@@ -1,9 +1,13 @@
+import string
+
+
 class Solution:
     def __init__(self, s: str):
         self.string = s
 
     def isAnagram(self, t: str) -> bool:
         charCount = {}
+
         for element in self.string:
             if element in charCount:
                 charCount[element] += 1
@@ -22,8 +26,22 @@ class Solution:
 
         return True
 
-    def isPlaindrome(self, t: str) -> bool:
-        pass
+    def isPlaindrome(self) -> bool:
+        valid_characters = string.ascii_letters + string.digits
+        potentialPalindrome = ("".join(ch for ch in self.string if ch in valid_characters)).lower()
+
+        i = 0
+        j = len(potentialPalindrome) - 1
+
+        while i < j:
+            if potentialPalindrome[i] == potentialPalindrome[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+
+        return True
+
 
 if __name__ == "__main__":
     # Anagram in O(n) time complexity
@@ -37,3 +55,18 @@ if __name__ == "__main__":
     t = "car"
     print("Given two strings {} and {} is a anagram ? {}"
           .format(s, t, Solution(s).isAnagram(t)))
+
+    s = ""
+    t = ""
+    print("Given two strings {} and {} is a anagram ? {}"
+          .format(s, t, Solution(s).isAnagram(t)))
+
+    # Printing isPalindrome
+    # In O(n) time complexity
+    s = "amanaplanacanalpanama"
+    print("Given string {} is a palindrome ? {}"
+          .format(s, Solution(s).isPlaindrome()))
+
+    s = "A man, a plan, a canal: Panama"
+    print("Given string {} is a palindrome ? {}"
+          .format(s, Solution(s).isPlaindrome()))
