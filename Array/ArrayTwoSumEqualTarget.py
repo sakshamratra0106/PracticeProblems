4f374b32d394aacc2cbe891cb20824f79aca6492
SAKSHAM <sakshamratra.0106@gmail.com>
2022-03-26 09:25:50 +0530

    Updates

--- Array/ArrayTwoSumEqualTarget.py
+++ Array/ArrayTwoSumEqualTarget.py
@@ -1,27 +1,26 @@
 # https://www.code-recipe.com/post/two-sum
 
 
-# Often, when dealing with iterators, we also get a need to keep a count of iterations. Python eases the programmers’ task by providing a built-in function enumerate() for this task.
-# Time Complexity: O(n)
+# Often, when dealing with iterators, we also get a need to keep a count of iterations. Python eases the programmers’
+# task by providing a built-in function enumerate() for this task. Time Complexity: O(n)
 #
 # Space Complexity: O(n)
-# Where as brute force with two loops it will take time complexity to O(n^2)
+# Whereas brute force with two loops it will take time complexity to O(n^2)
 from typing import List
 
 
-def sumOfTwoNumbers(nums : List[int], target: int):
-    seen={}
-    for i,element in enumerate(nums):
+def sumOfTwoNumbers(nums: List[int], target: int):
+    seen = {}
+    for i, element in enumerate(nums):
         difference = target - element
         if difference in seen:
             return seen[difference], i
 
-        seen[element]=i
+        seen[element] = i
 
 
 if __name__ == '__main__':
-
-    #Driver Program
+    # Driver Program
     arr2 = [31, -41, 59, 26, -53, 58, 97, -93, -23, 84]
 
-    print("In Array",arr2," Sum of 10 is available at index",sumOfTwoNumbers(arr2,-10))
+    print("In Array", arr2, " Sum of 10 is available at index", sumOfTwoNumbers(arr2, -10))
--- Array/IntersectionOfArrays.py
+++ Array/IntersectionOfArrays.py
@@ -1,6 +1,16 @@
 from typing import List
 
+
 def intersectOfArray(nums1: List[int], nums2: List[int]) -> List[int]:
+
+    """
+    :param nums1: List[int]
+    :param nums2: List[int]
+    :return: List[int]
+
+    Calculates the Intersection of two array in O(N) time complexity and O(N) Space complexity
+    """
+
     unique_elements_of_nums1 = {}
     common_elements = []
     for element in nums1:
@@ -16,10 +26,10 @@ def intersectOfArray(nums1: List[int], nums2: List[int]) -> List[int]:
 
     return common_elements
 
+
 if __name__ == '__main__':
-    #DriverCode
+    # DriverCode
     nums1 = [1, 2, 3, 4, 5]
     nums2 = [2, 5, 6]
     print("Intersection of the two arrays nums1 : {} and nums2 : {} is : {}"
-          .format(nums1,nums2, intersectOfArray(nums1,nums2)))
-
+          .format(nums1, nums2, intersectOfArray(nums1, nums2)))
