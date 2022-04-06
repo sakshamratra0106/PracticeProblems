d960d2efa9149eb75650e60b3e2555518cd81df3
SakshamRatra <sakshamratra.0106@gmail.com>
2022-04-06 07:27:35 +0530

    IntersectionOfArrays.py

--- Array/IntersectionOfArrays.py
+++ Array/IntersectionOfArrays.py
@@ -1,3 +1,16 @@
+"""Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+
+Follow up:
+
+1. What if the given array is already sorted? How would you optimize your algorithm?
+2. What if nums1's size is small compared to nums2's size?
+3. Which algorithm is better? What if elements of nums2 are stored on disk, and the memory is
+limited such that you cannot load all elements into the memory at once? """
+
 from typing import List
 
 
@@ -27,9 +40,49 @@ def intersectOfArray(nums1: List[int], nums2: List[int]) -> List[int]:
     return common_elements
 
 
+def intersectOfArray1(nums1: List[int], nums2: List[int]) -> List[int]:
+
+    """
+    :param nums1: List[int]
+    :param nums2: List[int]
+    :return: List[int]
+
+    1. What if the given array is already sorted? How would you optimize your algorithm?
+    TC : O(N)
+    SC : O(N)
+    """
+
+    length1 = len(nums1)
+    length2 = len(nums2)
+
+    if length1 == 0 or length2 == 0:
+        return []
+
+    i = 0
+    j = 0
+    common_elements = []
+    while i < length1 and j < length2:
+        if nums1[i] < nums2[j]:
+            i += 1
+        elif nums1[i] > nums2[j]:
+            j += 1
+        elif nums1[i] == nums2[j]:
+            common_elements.append(nums1[i])
+            i += 1
+            j += 1
+
+
+    return common_elements
+
+
 if __name__ == '__main__':
     # DriverCode
-    nums1 = [1, 2, 3, 4, 5]
+    nums1 = [1,  3, 4, 5, 2]
     nums2 = [2, 5, 6]
     print("Intersection of the two arrays nums1 : {} and nums2 : {} is : {}"
           .format(nums1, nums2, intersectOfArray(nums1, nums2)))
+
+    nums1 = [1, 2, 3, 4, 5]
+    nums2 = [2, 5, 6]
+    print("Intersection of the two arrays nums1 : {} and nums2 : {} is : {}"
+          .format(nums1, nums2, intersectOfArray1(nums1, nums2)))
