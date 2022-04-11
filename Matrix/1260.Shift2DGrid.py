de0d5517435612006253e8b1dfa48cff036cae07
SakshamRatra <sakshamratra.0106@gmail.com>
2022-04-11 17:52:07 +0530

    1260.Shift2DGrid.py

---
+++ Matrix/1260.Shift2DGrid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        if (k := k%(m*n)) == 0:  # after k shifts, final array = grid
+            return grid
+        div = gcd(m*n, k)  # math.gcd
+        for i in range(div):
+            r, c = divmod(i, n)
+            curr = grid[r][c]  # obtain initial element
+            for j in range(m*n//div):
+                r, c = divmod((i+k*(j+1))%(m*n), n)  # get the resultant index
+                grid[r][c], curr = curr, grid[r][c]  # perform swap operation
+        return grid
