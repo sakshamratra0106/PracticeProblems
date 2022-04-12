f1dfe2b0fe5caaeba172772a483b2aa1bd03e8ea
SakshamRatra <sakshamratra.0106@gmail.com>
2022-04-12 14:30:25 +0530

    Tree.py Refactored

--- Tree/Tree.py
+++ Tree/Tree.py
@@ -2,29 +2,39 @@ from typing import Optional
 
 
 class TreeNode:
-    def __init__(self, val: int):
+    def __init__(self, val):
         self.left = None
         self.val = val
         self.right = None
+        self.parent = None
 
 
 # Implementing Binary Search Tree
 class BinarySearchTree:
-    def __init__(self, root=None):
-        self.root = root
+    def __init__(self, head=None):
+        self.head = head
 
-    # Insert the value to the Tree
+    # Inserting values to the binary search tree
     def insert(self, value: int):
+        """
 
+        :param value:
+        :return:
+
+        TC : O(N) [If binary tree is skewed]
+        TC : O(LogN)
+        SC : O(1)
+
+        """
         # Create treenode using the inserted value
         n = TreeNode(value)
 
         y = None
-        temp = self.root
+        temp = self.head
 
         # finding the parent node
         # where new can become a child node
-        while temp is not None:
+        while temp:
             y = temp
             if n.val < temp.val:
                 temp = temp.left
@@ -35,33 +45,34 @@ class BinarySearchTree:
         # n.parent = y
 
         # Assign the new child node to the new paren node
-        # Or to root itself if the tree was empty before
-        if y is None:  # newly added node is root
-            self.root = n
+        # Or to head itself if the tree was empty before
+        if y is None:  # newly added node is head
+            self.head = n
         elif n.val < y.val:
             y.left = n
         else:
             y.right = n
 
-    # Inorder traversal of the tree left, root, right
     def inOrderTraversal(self, node: Optional[TreeNode]):
-        resultedTree = []
+
         if node:
-            resultedTree = self.inOrderTraversal(node.left)
-            resultedTree.append(node.val)
-            resultedTree = resultedTree + self.inOrderTraversal(node.right)
+            self.inOrderTraversal(node.left)
+            print(node.val, end=",")
+            self.inOrderTraversal(node.right)
 
-        return resultedTree
-
-    # PreOrder traversal of the tree root, left, right
     def preOrderTraversal(self, node: Optional[TreeNode]):
-        resultedTree = []
-        if node:
-            resultedTree.append(node.val)
-            resultedTree = resultedTree + self.preOrderTraversal(node.left)
-            resultedTree = resultedTree + self.preOrderTraversal(node.right)
 
-        return resultedTree
+        if node:
+            print(node.val, end=",")
+            self.preOrderTraversal(node.left)
+            self.preOrderTraversal(node.right)
+
+    def postOrderTraversal(self, node: Optional[TreeNode]):
+
+        if node:
+            self.postOrderTraversal(node.left)
+            self.postOrderTraversal(node.right)
+            print(node.val, end=",")
 
 
 if __name__ == "__main__":
@@ -75,10 +86,14 @@ if __name__ == "__main__":
     btree.insert(60)
     btree.insert(80)
 
-    print("Binary Search Tree Inorder Traversal after inserting is {}".format(
-        btree.inOrderTraversal(btree.root)
+    print("\n Binary Search Tree Inorder Traversal after inserting is {}".format(
+        btree.inOrderTraversal(btree.head)
     ))
 
-    print("Binary Search Tree Pre-Order Traversal after inserting is {}".format(
-        btree.preOrderTraversal(btree.root)
+    print("\n Binary Search Tree Inorder Traversal after inserting is {}".format(
+        btree.preOrderTraversal(btree.head)
+    ))
+
+    print("\n Binary Search Tree Inorder Traversal after inserting is {}".format(
+        btree.postOrderTraversal(btree.head)
     ))
